c93e586b298af187cb8672c7ec01549a1414b4c2
felpi <felipebmagagnin#gmail.com>
2023-07-14 11:11:31 -0300

    feat: add products listing ( dummy )

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,11 +4,15 @@ import Link from "next/link";
 
 export default function NavBar() {
     return (
-        <div class="placeholder-nav">
-            <Link href="/" class="active">Loja</Link>
+        <div className="placeholder-nav" style={{color: 'white'}}>
+            <Link href="/" className="active">Loja</Link>
             <Link href="/produtos">produtos</Link>
             <Link href="#contact">Contact</Link>
             <Link href="#about">About</Link>
+
+            <Link href='/'>
+                Cart With 0 items
+            </Link>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function Page() {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products').then(data => {
+            console.log(data)
+            setProducts(data.data.products)
+        })
+    }, [])
+
+    function addToCart(item){
+
+    }
+
     return (
         <>
-            <div>Home Page</div>
+            <div className="product-card-container">
+                {products.map((product, index) => {
+                    return (
+                        <div className="product-card" key={index}>
+                            <img src={product.thumbnail} alt={'product ' + product.title} className="product-thumbnail"/>
+                            
+                            Nome: {product.title}
+                            <br/>
+                            Marca: {product.brand}
+                            <br/>
+                            R$ {product.price}
+                            <br/>
+                            Desconto: {product.discountPercentage} %
+                            <br/>
+                            Avaliação: {product.rating}
+                            <br/>
+                            Estoque: {product.stock}
+                            <br/>
+                            <button>Comprar</button>
+                        </div>
+                    )
+                })}
+            </div>
+            
         </>
     )
 }
